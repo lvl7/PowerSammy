cf8a6e8c1f4100af2d46c20b3707a063af10e6ae
dzikakulka <tofix112@gmail.com>
2016-05-11 15:00:57 +0200

    qr

--- src/qr.cpp
+++ src/qr.cpp
@@ -1,26 +1,30 @@
 #include <ros/ros.h>
-#include <string>
+//#include <string>
 //#include <sensor_msgs/LaserScan.h>
+//#include <cstdlib>
+
 #include <geometry_msgs/Twist.h>
 #include <std_msgs/String.h>
 
-void qrMessReceived(std::string);
+void qrMessReceived(std_msgs::String);
 
 ros::NodeHandle node;
 ros::Publisher pub = node.advertise<geometry_msgs::Twist>("/rosaria/cmd_vel", 100);
 ros::Subscriber sub = node.subscribe("/visp_auto_tracker/code_message", 100, &qrMessReceived);
 
 
-void qrMessReceived(std::string msg)
+void qrMessReceived(std_msgs::String msg)
 {
-	std::string git = "https://github.com/lvl7/PowerSammy/wiki";
-	ROS_INFO_STREAM(msg);
-	geometry_msgs::Twist vel;
-    vel.linear.x = vel.linear.y = vel.linear.z = 0;
-    vel.angular.x = vel.angular.y = vel.angular.z = 0;
+	//std::string git = "https://github.com/lvl7/PowerSammy/wiki";
+//	std_msgs::String gitS;
+//	gitS.data = git;
+//	ROS_INFO_STREAM(msg);
+//	geometry_msgs::Twist vel;
+	//   vel.linear.x = vel.linear.y = vel.linear.z = 0;
+//    vel.angular.x = vel.angular.y = vel.angular.z = 0;
 	
-	if(msg == git) vel.linear.x = 0.3;
-	pub.publish(msg);
+//	if(gitS.data == msg.data) vel.linear.x = 0.3;
+//	pub.publish(msg);
 	
 }
 
